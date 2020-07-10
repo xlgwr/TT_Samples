68933311541c9cf9c99b9303dd37b60b000b3cfe
Josh Bramlett <bramsquad@mac.com>
2020-07-10 16:43:50 -0500

    Add support to change weights

--- TT_NET_CLIENT_SIDE/TTNETAPI_Sample_WPF_VolumeRatio/MainWindow.xaml.cs
+++ TT_NET_CLIENT_SIDE/TTNETAPI_Sample_WPF_VolumeRatio/MainWindow.xaml.cs
@@ -3,44 +3,124 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
 using tt_net_sdk;
+using System.Text.RegularExpressions;
 
 namespace TTNETAPI_Sample_WPF_VolumeRatio
 {
     #region TreeRow Implementation
     public interface ITreeRow : INotifyPropertyChanged
     {
+        string MarketName { get; }
         string ProductName { get; }
         string ContractAlias { get; }
         decimal Volume { get; }
+        long NumNewOrders { get; }
+        long NumModifications { get; }
+        long NumCancellations { get; }
         int Score { get; }
         decimal Ratio { get; }
         bool IsExpanded { get; set; }
     }
 
+    public class MarketTreeRow : ITreeRow
+    {
+        public MarketTreeRow(Instrument instr)
+        {
+            m_market = instr.Product.Market;
+            IsExpanded = false;
+            CreateOrGetExistingChild(instr);
+        }
+
+        #region Tree Column Properties
+        public string MarketName => m_market.Name;
+        public string ProductName => string.Empty;
+        public string ContractAlias => string.Empty;
+        public long NumNewOrders => this.Children.Sum(x => x.NumNewOrders);
+        public long NumModifications => this.Children.Sum(x => x.NumModifications);
+        public long NumCancellations => this.Children.Sum(x => x.NumCancellations);
+        public decimal Volume => this.Children.Sum(x => x.Volume);
+        public int Score => this.Children.Sum(x => x.Score);
+        public decimal Ratio => (this.Volume == 0) ? 0 : this.Score / this.Volume;
+        #endregion
+
+        #region TreeViewItem Methods/Properties
+        public MarketId MarketId => m_market.MarketId;
+        public ObservableCollection<ITreeRow> Children { get; } = new ObservableCollection<ITreeRow>();
+
+        public bool IsExpanded
+        {
+            get { return m_isExpanded; }
+            set
+            {
+                m_isExpanded = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Public Methods
+        public ProductTreeRow CreateOrGetExistingChild(Instrument instr)
+        {
+            var child = this.Children.FirstOrDefault(x => x.ProductName == instr.Product.Key.Name);
+            if (child == null)
+            {
+                this.Children.Add(new ProductTreeRow(this, instr));
+                child = this.Children.Last();
+
+                child.PropertyChanged += (o, args) =>
+                {
+                    NotifyPropertyChanged(args.PropertyName);
+                    NotifyPropertyChanged("Score");
+                    NotifyPropertyChanged("Ratio");
+                };
+            }
+
+            return child as ProductTreeRow;
+        }
+
+        public void ResetWeights(int nos_weight, int chg_weight, int cxl_weight)
+        {
+            foreach (var child in Children)
+            {
+                var contractRow = child as ProductTreeRow;
+                contractRow.ResetWeights(nos_weight, chg_weight, cxl_weight);
+            }
+        }
+        #endregion
+
+        #region Private Members
+        private readonly Market m_market;
+        private bool m_isExpanded = false;
+        #endregion
+    }
+
     public class ProductTreeRow : ITreeRow
     {
-        public ProductTreeRow(Instrument instr)
+        public ProductTreeRow(MarketTreeRow parent, Instrument instr)
         {
+            m_parent = parent;
             m_productKey = instr.Product.Key;
             CreateOrGetExistingChild(instr);
         }
 
         #region Tree Column Properties
+        public string MarketName => string.Empty;
         public string ProductName => m_productKey.Name;
         public string ContractAlias => string.Empty;
         public decimal Volume => this.Children.Sum(x => x.Volume);
+        public long NumNewOrders => this.Children.Sum(x => x.NumNewOrders);
+        public long NumModifications => this.Children.Sum(x => x.NumModifications);
+        public long NumCancellations => this.Children.Sum(x => x.NumCancellations);
         public int Score => this.Children.Sum(x => x.Score);
         public decimal Ratio => (this.Volume == 0) ? 0 : this.Score / this.Volume;
         #endregion
@@ -55,15 +135,14 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
             set
             {
                 m_isExpanded = value;
-                this.OnPropertyChanged("IsExpanded");
+                this.NotifyPropertyChanged("IsExpanded");
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
-            if (this.PropertyChanged != null)
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
 
@@ -75,13 +154,30 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
             {
                 this.Children.Add(new ContractTreeRow(this, instr));
                 child = this.Children.Last();
+
+                child.PropertyChanged += (o, args) =>
+                {
+                    NotifyPropertyChanged(args.PropertyName);
+                    NotifyPropertyChanged("Score");
+                    NotifyPropertyChanged("Ratio");
+                };
             }
 
             return child as ContractTreeRow;
         }
+
+        public void ResetWeights(int nos_weight, int chg_weight, int cxl_weight)
+        {
+            foreach(var child in Children)
+            {
+                var contractRow = child as ContractTreeRow;
+                contractRow.ResetWeights(nos_weight, chg_weight, cxl_weight);
+            }
+        }
         #endregion
 
         #region Private Members
+        private readonly MarketTreeRow m_parent = null;
         private readonly ProductKey m_productKey = ProductKey.Empty;
         private bool m_isExpanded = false;
         #endregion
@@ -97,12 +193,53 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
         }
 
         #region Tree Column Properties
+        public string MarketName => string.Empty;
         public string ProductName => string.Empty;
         public string ContractAlias => m_instrKey.Alias;
-        public decimal Volume => m_volume;
-        public int Score => (m_numNewOrders * NEW_ORDER_MULTIPLIER) + 
-                            (m_numModifications * MODIFY_MULTIPLIER) + 
-                            (m_numCancellations * CANCEL_MULTIPLIER);
+
+        public decimal Volume
+        {
+            get { return m_volume; }
+            set
+            {
+                m_volume = value;
+                NotifyRatioChanged();
+            }
+        }
+
+        public long NumNewOrders
+        {
+            get { return m_numNewOrders; }
+            set
+            {
+                m_numNewOrders = value;
+                NotifyRatioChanged();
+            }
+        }
+
+        public long NumModifications
+        {
+            get { return m_numModifications; }
+            set
+            {
+                m_numModifications = value;
+                NotifyRatioChanged();
+            }
+        }
+
+        public long NumCancellations
+        {
+            get { return m_numCancellations; }
+            set
+            {
+                m_numCancellations = value;
+                NotifyRatioChanged();
+            }
+        }
+
+        public int Score => (int)(m_numNewOrders * m_nosWeight) +
+                            (int)(m_numModifications * m_chgWeight) +
+                            (int)(m_numCancellations * m_cxlWeight);
         public decimal Ratio => (this.Volume == 0) ? 0 : this.Score / this.Volume;
         #endregion
 
@@ -116,48 +253,68 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
             set
             {
                 m_isExpanded = value;
-                this.OnPropertyChanged("IsExpanded");
+                NotifyPropertyChanged();
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
-            if (this.PropertyChanged != null)
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        private void NotifyRatioChanged([CallerMemberName] String baseProperty = "")
+        {
+            if (baseProperty != "")
+            {
+                NotifyPropertyChanged(baseProperty);
+            }
+            
+            NotifyPropertyChanged("Score");
+            NotifyPropertyChanged("Ratio");
+        }
+
         #endregion
 
         #region Public Methods
         public void ProcessMessage(OrderAddedEventArgs e)
         {
-            m_numNewOrders++;
+            NumNewOrders++;
         }
 
         public void ProcessMessage(OrderUpdatedEventArgs e)
         {
-            m_numModifications++;
+            NumModifications++;
         }
 
         public void ProcessMessage(OrderDeletedEventArgs e)
         {
-            m_numCancellations++;
+            NumCancellations++;
         }
 
         public void ProcessMessage(OrderFilledEventArgs e)
         {
-            m_volume += e.Fill.Quantity.Value;
+            Volume += e.Fill.Quantity.Value;
+        }
+
+        public void ResetWeights(int nos_weight, int chg_weight, int cxl_weight)
+        {
+            m_nosWeight = nos_weight;
+            m_chgWeight = chg_weight;
+            m_cxlWeight = cxl_weight;
+            NotifyRatioChanged();
         }
         #endregion
 
         #region Private Members
-        private static int NEW_ORDER_MULTIPLIER = 0;
-        private static int MODIFY_MULTIPLIER = 1;
-        private static int CANCEL_MULTIPLIER = 3;
+        private int m_nosWeight = 0;
+        private int m_chgWeight = 1;
+        private int m_cxlWeight = 3;
+
         private decimal m_volume = 0;
-        private int m_numNewOrders = 0;
-        private int m_numModifications = 0;
-        private int m_numCancellations = 0;
+        private long m_numNewOrders = 0;
+        private long m_numModifications = 0;
+        private long m_numCancellations = 0;
 
         private readonly InstrumentKey m_instrKey = InstrumentKey.Empty;
         private readonly ProductTreeRow m_parent = null;
@@ -165,9 +322,9 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
         #endregion
     }
 
-    public class ProductList : ObservableCollection<ProductTreeRow>
+    public class MarketList : ObservableCollection<MarketTreeRow>
     {
-        public ProductList() : base() { }
+        public MarketList() : base() { }
     }
     #endregion
 
@@ -176,23 +333,26 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ProductList ProductListData = null;
         private TTAPI m_api = null;
         private tt_net_sdk.Dispatcher m_dispatcher = null;
         private TradeSubscription m_ts = null;
         private bool m_shutdownComplete = false;
 
+        private MarketList MarketListData = new MarketList();
+        public int NOS_Weight { get; set; } = 0;
+        public int CHG_Weight { get; set; } = 1;
+        public int CXL_Weight { get; set; } = 3;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            ProductListData = new ProductList();
-            treeviewList.ItemsSource = ProductListData;
+            this.DataContext = this;
+            treeviewList.ItemsSource = MarketListData;
 
             var app = App.Current as TTNETAPI_Sample_WPF_VolumeRatio.App;
             m_dispatcher = app.SDKDispatcher;
 
-            var app_key = "";
+            var app_key = "app_key";
             var env = ServiceEnvironment.ProdSim;
             var mode = TTAPIOptions.SDKMode.Client;
             var options = new TTAPIOptions(mode, env, app_key, 5000);
@@ -258,7 +418,6 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
             if (row != null)
             {
                 row.ProcessMessage(e);
-                CollectionViewSource.GetDefaultView(this.ProductListData).Refresh();
             }
         }
 
@@ -268,7 +427,6 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
             if (row != null)
             {
                 row.ProcessMessage(e);
-                CollectionViewSource.GetDefaultView(this.ProductListData).Refresh();
             }
         }
 
@@ -278,17 +436,15 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
             if (row != null)
             {
                 row.ProcessMessage(e);
-                CollectionViewSource.GetDefaultView(this.ProductListData).Refresh();
             }
         }
 
         private void OnOrderFilled(object sender, OrderFilledEventArgs e)
         {
-            var row = GetContractRow(e.NewOrder.Instrument);
+            var row = GetContractRow(e.Fill.Instrument);
             if (row != null)
             {
                 row.ProcessMessage(e);
-                CollectionViewSource.GetDefaultView(this.ProductListData).Refresh();
             }
         }
 
@@ -312,24 +468,50 @@ namespace TTNETAPI_Sample_WPF_VolumeRatio
                 Shutdown();
             }
         }
+
+        private static readonly Regex s_numericOnly = new Regex("[^0-9.-]+");
+        private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = s_numericOnly.IsMatch(e.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNOSWeight.Text))
+                    return;
+                if (string.IsNullOrWhiteSpace(txtCHGWeight.Text))
+                    return;
+                if (string.IsNullOrWhiteSpace(txtCXLWeight.Text))
+                    return;
+
+                var nos_weight = Int32.Parse(txtNOSWeight.Text);
+                var chg_weight = Int32.Parse(txtCHGWeight.Text);
+                var cxl_weight = Int32.Parse(txtCXLWeight.Text);
+                foreach(var parent in MarketListData)
+                {
+                    var parentRow = parent as MarketTreeRow;
+                    parentRow.ResetWeights(nos_weight, chg_weight, cxl_weight);
+                }
+            }
+            catch { }
+        }
         #endregion
 
         #region Private Methods
         private ContractTreeRow GetContractRow(Instrument instr)
         {
-            if (instr.Key.MarketId != MarketId.CME)
-            {
-                return null;
-            }
-
-            var parent = this.ProductListData.FirstOrDefault(x => x.ProductId == instr.Product.Key.ProductId);
+            var parent = this.MarketListData.FirstOrDefault(x => x.MarketId == instr.Product.Market.MarketId);
             if (parent == null)
             {
-                this.ProductListData.Add(new ProductTreeRow(instr));
-                parent = this.ProductListData.Last();
+                this.MarketListData.Add(new MarketTreeRow(instr));
+                parent = this.MarketListData.Last();
             }
 
-            return parent.CreateOrGetExistingChild(instr);
+            var productRow = parent.CreateOrGetExistingChild(instr);
+            var contractRow = productRow.CreateOrGetExistingChild(instr);
+            return contractRow;
         }
 
         private void Shutdown()
